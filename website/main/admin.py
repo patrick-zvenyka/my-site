825ea809371454e52cff2b08c3edd8165b2cb7b7
patrick-zvenyka <patrickzvenyika1999@gmail.com>
2025-12-24 06:56:53 +0200

    admin-logo

--- website/main/admin.py
+++ website/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Certification)
+admin.site.register(Project)
--- website/website/settings.py
+++ website/website/settings.py
@@ -142,7 +142,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "My Site",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "books/img/logo.png",
+    "site_logo": "img/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
